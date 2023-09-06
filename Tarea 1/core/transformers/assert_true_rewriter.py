ea922462a950dec165428734b92538e81a34e81c
JuanIke <jmbarra@uc.cl>
2023-09-06 12:51:03 -0300

    2.1

--- Tarea 1/core/transformers/assert_true_rewriter.py
+++ Tarea 1/core/transformers/assert_true_rewriter.py
@@ -3,8 +3,24 @@ import ast
 from core.rewriter import RewriterCommand
 
 
+class AssertTrueTransformer(NodeTransformer):
+    def visit_Call(self, node):
+        NodeTransformer.generic_visit(self, node)
+        if node.func.attr == 'assertEquals':
+            return Call(
+            func=Attribute(
+               value=Name(id='self', ctx=Load()),
+               attr='assertTrue',
+               ctx=Load()),
+            args=[node.args[0]],
+            keywords=[])
+        else:
+            return node
+        
+
 class AssertTrueCommand(RewriterCommand):
     # Implementar comando, recuerde que puede necesitar implementar además clases NodeTransformer y/o NodeVisitor.
 
-    def apply(self, node):
-        pass
+    def apply(self, tree):
+        new_tree = fix_missing_locations(AssertTrueTransformer().visit(tree))
+        return new_tree
