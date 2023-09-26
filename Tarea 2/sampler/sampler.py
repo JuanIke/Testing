01fbc4e65026ed01b9ab75cd187e590c1ef0b429
MariaBarias <mcbarias@uc.cl>
2023-09-25 22:06:01 -0300

    falta escribir como arbolito

--- Tarea 2/sampler/sampler.py
+++ Tarea 2/sampler/sampler.py
@@ -10,6 +10,8 @@ class Sampler:
         self.tid = tid
         self.t = threading.Thread(target=self.sample, args=())
         self.active = True
+        self.final_tree = [] # [function_name, seg, activa(bool), pos] 
+        
         
     def start(self):
         self.active = True
@@ -29,6 +31,31 @@ class Sampler:
                     stack.append(code)
                 stack.reverse()
                 print(stack)  # Esta linea imprime el stack despues de invertirlo la pueden comentar o descomentar si quieren
+                
+                for pos in range(len(stack)):
+                    self.usado = False
+                    for fun in self.final_tree: 
+                        if stack[pos] == fun[0] and fun[2]:
+                            self.usado = True
+                            fun[1] += 1
+                            break
+                    if self.usado == False:
+                        self.final_tree.append([stack[pos], 1, True, pos])
+
+                contador_pos_stack = 0
+                contador_pos_tree = 0
+                for item in self.final_tree:
+                    if contador_pos_stack == len(stack):
+                        break
+                    if item[2] and item[0] == stack[contador_pos_stack]:
+                        contador_pos_stack += 1
+                    else:
+                        break
+                    contador_pos_tree += 1
+
+                for item_num in range(contador_pos_tree, len(self.final_tree)):
+                    self.final_tree[item_num][2] = False
+                    
     
     def sample(self):
         while self.active:
@@ -36,5 +63,4 @@ class Sampler:
             sleep(1)
 
     def printReport(self):
-        # Este metodo debe imprimir el reporte del call context tree
-        pass
+        print("\nFinal: ",self.final_tree, "\n")
