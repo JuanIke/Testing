78c6ff919f1d9437a1efc0d2ed7b7c27630aa609
MariaBarias <mcbarias@uc.cl>
2023-09-26 23:40:49 -0300

    se agrega parentesco, falta revisar 1 seg de diferencia

--- Tarea 2/sampler/sampler.py
+++ Tarea 2/sampler/sampler.py
@@ -10,7 +10,7 @@ class Sampler:
         self.tid = tid
         self.t = threading.Thread(target=self.sample, args=())
         self.active = True
-        self.final_tree = [] # [function_name, seg, activa(bool), pos] 
+        self.final_tree = [] # [function_name, seg, pos, parents] 
         
         
     def start(self):
@@ -33,30 +33,30 @@ class Sampler:
                 print(stack)  # Esta linea imprime el stack despues de invertirlo la pueden comentar o descomentar si quieren
                 
                 # desactivar antes de pasar nuevos nodos
-                contador_pos_stack = 0
-                contador_pos_tree = 0
-                for item in self.final_tree:
-                    if contador_pos_stack == len(stack):
-                        break
-                    if item[2] and item[0] == stack[contador_pos_stack]:
-                        contador_pos_stack += 1
-                    elif item[2] and item[0] != stack[contador_pos_stack]:
-                        break
-                    contador_pos_tree += 1
+                # contador_pos_stack = 0
+                # contador_pos_tree = 0
+                # for item in self.final_tree:
+                #     if contador_pos_stack == len(stack):
+                #         break
+                #     if item[2] and item[0] == stack[contador_pos_stack]:
+                #         contador_pos_stack += 1
+                #     elif item[2] and item[0] != stack[contador_pos_stack]:
+                #         break
+                #     contador_pos_tree += 1
 
-                for item_num in range(contador_pos_tree, len(self.final_tree)):
-                    self.final_tree[item_num][2] = False
+                # for item_num in range(contador_pos_tree, len(self.final_tree)):
+                #     self.final_tree[item_num][2] = False
 
                 # agregar los nuevos nodos
                 for pos in range(len(stack)):
                     self.usado = False
                     for fun in self.final_tree: 
-                        if stack[pos] == fun[0] and fun[2] and pos == fun[3]:
+                        if stack[pos] == fun[0] and pos == fun[2] and fun[3] == stack[0:pos] :
                             self.usado = True
                             fun[1] += 1
                             break
                     if self.usado == False:
-                        self.final_tree.append([stack[pos], 1, True, pos])
+                        self.final_tree.append([stack[pos], 1, pos, stack[0:pos]])
 
                 # Desactivar funciones ACA hay que modificar (ver si hacer un mas 1 al pos_tree para dar espacio)
                     
